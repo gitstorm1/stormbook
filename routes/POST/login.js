cc088ced7450858ae54139b1649680ec3b1bd3c8
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2024-12-19 10:21:06 +0500

    Extracted functions in login.js

--- routes/POST/login.js
+++ routes/POST/login.js
@@ -3,28 +3,38 @@ import { db } from '../../db.js';
 import bcrypt from 'bcrypt';
 
 export default async function (req, res) {
-    if (req.session.user) return res.status(400).end();
+    if (isUserLoggedIn(req)) return res.status(409).end();
 
     const email = req.body.email;
-    const password = req.body.password;
+    const enteredPassword = req.body.password;
 
-    const queryResult = await db.oneOrNone('SELECT id, pwd_hash FROM users WHERE email=$1 LIMIT 1', [email]);
+    const accountIDAndHash = await getAccountIDAndHashFromEmail(email);
 
-    if (queryResult === null) {
-        return res.send('No account of the specified email exists');
+    if (accountIDAndHash === null) {
+        return res.status(404).send('No account of the specified email exists');
     }
 
-    const correctPassword = await bcrypt.compare(password, queryResult.pwd_hash);
-
-    if (!correctPassword) {
-        return res.send('Incorrect password');
+    if (!(await isEnteredPasswordCorrect(enteredPassword, accountIDAndHash.pwd_hash))) {
+        return res.status(401).send('Incorrect password');
     }
 
     req.session.user = {
-        id: queryResult.id,
+        id: accountIDAndHash.id,
     };
 
     console.log('Logged in successfully');
 
     res.redirect('/');
+}
+
+function isUserLoggedIn(req) {
+    return (req.session.user !== undefined);
+}
+
+async function getAccountIDAndHashFromEmail(email) {
+    return await db.oneOrNone('SELECT id, pwd_hash FROM users WHERE email=$1 LIMIT 1', [email]);
+}
+
+async function isEnteredPasswordCorrect(enteredPassword, passwordHash) {
+    return await bcrypt.compare(enteredPassword, passwordHash);
 }
