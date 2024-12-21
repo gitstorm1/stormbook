3b993772b78fb06d821a2df678ec5c5672b19cab
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2024-12-21 08:35:49 +0500

    Cleaned up logout-get.js

--- users/logout-get.js
+++ users/logout-get.js
@@ -1,12 +1,14 @@
-async function initialize(app) {
-    app.post('/logout', async function (req, res) {
-        if (!req.session.user) return res.status(401).end();
-        
-        req.session.regenerate((err) => {
-            if (!err) return;
-            console.error(err);
-        });
+async function logoutHandler(req, res) {
+    if (!req.session.user) return res.status(401).end();
+    
+    req.session.regenerate((err) => {
+        if (!err) return;
+        console.error(err);
     });
+}
+
+async function initialize(app) {
+    app.post('/logout', logoutHandler);
 }
 
 export default { initialize: initialize };
