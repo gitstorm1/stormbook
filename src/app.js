cebeeb0036d7b156a1503c6aa58be62c42c086fb
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2024-12-26 12:30:44 +0500

    Progress on login

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 import express from 'express';
 
+import bcrypt from 'bcrypt';
+
 export function createApp(database) {
     const app = express();
 
+    app.use(express.json());
+
     app.post('/api/users/auth/login', async (req, res) => {
+        const enteredEmail = req.body.email;
+        const enteredPassword = req.body.password;
+
+        const accountIdAndHash = database.getAccountIdAndHashFromEmail(enteredEmail);
+
+        const accountExists = (accountIdAndHash !== null);
+
+        if (!accountExists) {
+            return res.status(401).send('Incorrect email');
+        }
+
+        if (!(await bcrypt.compare(enteredPassword, accountIdAndHash.pwd_hash))) {
+            return res.status(401).send('Incorrect password');
+        }
+
+        console.log("User logged in");
+
         res.json({});
+        //res.redirect('/');
     });
 
     return app;
--- src/app.test.js
+++ src/app.test.js
@@ -1,20 +1,30 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import assert from 'node:assert';
 
 import request from "supertest";
 
 import { createApp } from "./app.js";
 
-const app = createApp();
+const databaseMock = {
+    getAccountIdAndHashFromEmail: (email) => {
+        return {
+            id: 1,
+            pwd_hash: 'fake_hash',
+        }
+    }
+}
+
+const app = createApp(databaseMock);
 
 describe('Authentication', () => {
-    it('should login successfully', async () => {
+    it('should login successfully', async (done) => {
         const res = await request(app)
         .post('/api/users/auth/login')
         .send({
-            email: "testemail@example.com",
-            password: "testpassword123",
+            email: "abc@test.com",
+            password: "12345678",
         });
         assert.strictEqual(res.statusCode, 200);
+        done();
     });
 });
